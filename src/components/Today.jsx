1919b00abc6575629e9e04eb903084784074c420
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-10 17:11:12 -0600

    add components and imgs

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -4,6 +4,12 @@ export const Today = () => {
 
     return(<>
     <section className="today">
+        <div className="div-today-search">
+            <button>Seach for places</button>
+            <button></button>
+        </div>
+        <div></div>
+        <div></div>
         <h1>Hola mundo</h1>
     </section>
     </>)
