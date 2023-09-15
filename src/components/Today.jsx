58d4cf5eec33f72d195b1ff31c6417458e1163a6
RonyPadilla <ronymoisespadillacruz@gmail.com>
2023-09-15 12:26:39 -0600

    modified components

--- src/components/Today.jsx
+++ src/components/Today.jsx
@@ -12,7 +12,7 @@ export const Today = ({ sendSearch, sendCountry }) => {
 
    const weatherWeek = async function fetchData () {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c5d292e922b44584803144339231309&q=${sendCountry}&days=6&aqi=yes&alerts=no`)
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c5d292e922b44584803144339231309&q=${sendCountry}&days=6&aqi=yes&alerts=no`)
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
--- src/components/WeatherWeek.jsx
+++ src/components/WeatherWeek.jsx
@@ -11,7 +11,7 @@ export const WeatherWeek = ({ sendCountry }) => {
   console.log('this is he WeatherWeek ' + sendCountry)
   const weatherWeek = async function fetchData () {
     try {
-      const response = await fetch(`http://api.weatherapi.com/v1/forecast.json?key=c5d292e922b44584803144339231309&q=${sendCountry}&days=6&aqi=yes&alerts=no`)
+      const response = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=c5d292e922b44584803144339231309&q=${sendCountry}&days=6&aqi=yes&alerts=no`)
       if (!response.ok) {
         throw new Error('Network response was not ok')
       }
